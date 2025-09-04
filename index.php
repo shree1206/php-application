c85949bf9f6832895e38951b494a6a57a8c83570
Vaibhav <you@example.com>
2025-09-04 22:04:35 +0530

    restrict index page after login

--- index.php
+++ index.php
@@ -3,9 +3,18 @@ session_start();
 require_once __DIR__ . '/includes/config.php';
 require_once __DIR__ . '/includes/header.php';
 
-if (isset($_SESSION['role'])) {
-    $role = $_SESSION['role'];
-    print ($role);
+// Check if the user is logged in, if not then redirect to login page
+if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
+    header("location: login");
+    exit;
+} else {
+    if (isset($_SESSION['role']) && $_SESSION['role'] == 2) {
+        header("location: user");
+        exit;
+    } else {
+        header("location: admin");
+        exit;
+    }
 }
 
 require_once __DIR__ . '/includes/footer.php';
